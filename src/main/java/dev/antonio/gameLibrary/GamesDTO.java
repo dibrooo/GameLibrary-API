12cb134b23cbd41342288a13b4711439d35b5825
António Amaral <antonio.amaral.7778@gmail.com>
2024-02-02 02:29:03 -0100

    Replaced Games price type from Float to Double

--- src/main/java/dev/antonio/gameLibrary/GamesDTO.java
+++ src/main/java/dev/antonio/gameLibrary/GamesDTO.java
@@ -1,22 +1,12 @@
 package dev.antonio.gameLibrary;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
-import org.bson.types.ObjectId;
-import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.Date;
 import java.util.List;
 
-@Document(collection = "games", language = "java")
 @Data
-@AllArgsConstructor
-@NoArgsConstructor
-public class Games {
-    @Id
-    private ObjectId id;
+public class GamesDTO {
     private String name;
     private String description;
     private List<String> categories;
